0bad63a4f97dade28550e61aa64150a6a2a31578
timothypomeroy117 <150109892+timothypomeroy117@users.noreply.github.com>
2023-11-20 17:03:05 -0700

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,36 @@
+print ("Hello World")
+
+var1 = "none"
+Var2 = 2
+
+if var1 is "none":
+  print ("It is a none variable")
+else:
+    print ("It is not a none variable")
+
+occupation = ["Doctor", "Engineer", "Analyst"]
+for x in occupation:
+    print(x) 
+
+sarah = bob = mike = 17
+print (sarah)
+print (bob)
+
+name, age = "avi" , 22
+print (age)
+print (name)
+
+age1 = 12
+age2 = 18
+print(age1 + age2)
+
+firstname = "avi"
+lastname = "jane"
+print(firstname + " " + lastname)
+
+sent = ("avi was playing basketball")
+print(sent[0:3])
+
+name("jake")
+sentence = "%s is 21"
+print(sentence % name)
